162f5c4b37b84a4e88ddbcbe885fab4efa645676
Anne A <aaguirre.rdit@gmail.com>
2019-03-07 15:39:11 +0900

    category change and layout

--- src/App.js
+++ src/App.js
@@ -8,8 +8,30 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      currentCategory : productData[0].category,
+      currentMenuItems : productData[0].items,
+      currentOrder: [],
+      totalPrice: 0,
+
+
     }
   }
+  handleCategoryClick = (event) => {
+    const newCategory = event.target.textContent;
+    const newMenuItems = productData.filter((category)=>{
+      if (category.category === newCategory){
+        return category;
+      }
+    });
+
+    this.setState({
+      currentCategory: newCategory,
+      currentMenuItems: newMenuItems[0].items,
+    });
+  };
+  handleAddClick = (event) => {
+    console.log(event.target.parentNode)
+  };
   render() {
     return (
       <Grid
@@ -19,7 +41,13 @@ class App extends Component {
         justify="space-around"
         alignItems="stretch"
       >
-        <Menu className={'Products'} style={{flex:'1'}}>
+        <Menu className={'Products'}
+              style={{flex:'1'}}
+              currentCategory={this.state.currentCategory}
+              currentMenuItems={this.state.currentMenuItems}
+              handleCategoryClick={this.handleCategoryClick}
+              handleAddClick={this.handleAddClick}
+        >
         </Menu>
       <Sidebar style={{flex:'1'}}/>
       </Grid>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,16 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 import productData from '../productData';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
+const MenuCard = styled(Card)`
+  max-width:250px;
+  max-height:250px;
+  width:250px;
+  margin:10px;
+  padding:5px;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:space-evenly;
+`;
+const MenuImage = styled.img`
+  max-height: 80px;
+    width:auto;
+`;
+const FabButton = styled(Fab)`
+  font-size:
+`;
 export default class Menu extends React.Component{
   constructor(props){
     super(props)
   }
   render(){
     return(
-      <Grid item xs={8} className={'Products'}>
-      <h2>Categories</h2>
+      <Grid item xs={8}
+            container
+            justify="space-between"
+            alignItems="center"
+            className={'Products'}
+            direction={'column'}
+            >
         <Grid
           container
           spacing={16}
@@ -19,15 +46,47 @@ export default class Menu extends React.Component{
           direction={'row'}
           justify={'space-evenly'}
         >
-        {/*{productData.map((category) =>(
-          <Button variant="outlined" size="large" color="primary" className={'categoryBtn'}>
+        {productData.map((category) =>(
+          <Button variant={'contained'}
+                  size="large"
+                  color={this.props.currentCategory === category.category ? 'secondary': 'default'}
+                  className={'categoryBtn'}
+                  id={category.category}
+                  onClick={this.props.handleCategoryClick}
+          >
             {category.category}
           </Button>
           )
         )
-        }*/}
+        }
         </Grid>
-        <h3>Options</h3>
+        <Grid
+          container
+          spacing={16}
+          className={'Options'}
+          alignItems={'center'}
+          direction={'row'}
+          justify={'space-evenly'}
+        >
+          {this.props.currentMenuItems.map((item)=>(
+            <MenuCard>
+              <MenuImage src={item.img}/>
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+              <Fab color="secondary"
+                   aria-label="Add"
+                   variant={'extended'}
+                   size={'small'}
+                   onClick={this.props.handleAddClick}
+                   item={item.name}
+                   price={item.price}
+              >
+                <AddIcon />
+                추가
+              </Fab>
+            </MenuCard>
+          ))}
+        </Grid>
     </Grid>)
   }
 }
